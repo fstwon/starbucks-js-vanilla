aacd227f094ef01444f9b8c775de876e5d8fcc23
fstwon <fstwon@gmail.com>
2024-02-14 15:31:38 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,22 @@
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+
+const firstScriptTag = document.getElementsByTagName('script')[0];
+
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8',
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playList: 'An6LvWQuj_8'
+    },
+    events: {
+      onReady: (event) => {
+        event.target.mute();
+      }
+    }
+  });
+};
